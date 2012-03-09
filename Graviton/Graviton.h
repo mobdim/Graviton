4b525d88432b744ed484f3435b45eb4fc96a975f
Logan Collins <loganscollins@gmail.com>
2012-03-09 11:33:27 -0800

    Initial Commit

---
+++ Graviton/Graviton.h
@@ -0,0 +1,13 @@
+//
+//  Graviton.h
+//  Graviton
+//
+//  Created by Logan Collins on 3/9/12.
+//  Copyright (c) 2012 Sunflower Softworks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Graviton : NSObject
+
+@end
---
+++ Graviton/Graviton.m
@@ -0,0 +1,13 @@
+//
+//  Graviton.m
+//  Graviton
+//
+//  Created by Logan Collins on 3/9/12.
+//  Copyright (c) 2012 Sunflower Softworks. All rights reserved.
+//
+
+#import "Graviton.h"
+
+@implementation Graviton
+
+@end
