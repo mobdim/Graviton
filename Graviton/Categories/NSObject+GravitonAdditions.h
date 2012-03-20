1fba9fb0b6fa58f0e548ad530a78734a232d2f23
Logan Collins <loganscollins@gmail.com>
2012-03-20 15:35:34 -0700

    Added NSObject+GravitonAdditions

---
+++ Graviton/Categories/NSObject+GravitonAdditions.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+GravitonAdditions.h
+//  Graviton
+//
+//  Created by Logan Collins on 3/20/12.
+//  Copyright (c) 2012 Sunflower Softworks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (GravitonAdditions)
+
+@end
---
+++ Graviton/Categories/NSObject+GravitonAdditions.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+GravitonAdditions.m
+//  Graviton
+//
+//  Created by Logan Collins on 3/20/12.
+//  Copyright (c) 2012 Sunflower Softworks. All rights reserved.
+//
+
+#import "NSObject+GravitonAdditions.h"
+
+@implementation NSObject (GravitonAdditions)
+
+@end
