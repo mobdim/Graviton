18b1e8cea58c06e3932b7ce5bd4c47f3b817d9da
Logan Collins <logan@panic.com>
2012-08-31 17:11:53 -0700

    Documentation update.

--- Graviton/Categories/NSObject+GravitonAdditions.h
+++ Graviton/Categories/NSObject+GravitonAdditions.h
@@ -10,6 +10,16 @@
 #import <Graviton/GravitonDefines.h>
 
 
+/*!
+ * @enum GXAssociationPolicy
+ * @abstract Object association policies
+ * 
+ * @constant GXAssociationPolicyAssign              Assign the object without retaining it
+ * @constant GXAssociationPolicyRetainNonatomic     Retain the object without atomic guards
+ * @constant GXAssociationPolicyCopyNonatomic       Copy the object (with NSCopying) without atomic guards
+ * @constant GXAssociationPolicyRetain              Retain the object with atomic guards
+ * @abstract GXAssociationPolicyCopy                Copy the object (with NSCopying) with atomic guards
+ */
 typedef GRAVITON_ENUM(NSUInteger, GXAssociationPolicy) {
     GXAssociationPolicyAssign = 0,
     GXAssociationPolicyRetainNonatomic = 1,
@@ -21,10 +31,103 @@ typedef GRAVITON_ENUM(NSUInteger, GXAssociationPolicy) {
 
 @interface NSObject (GravitonAdditions)
 
-- (void)gx_performBlock:(void (^)(void))block;
+/*!
+ * @method gx_performAfterDelay:block:
+ * @abstract Invokes a block object after a delay
+ * 
+ * @param delay
+ * The time interval to wait
+ * 
+ * @param block
+ * The block to perform
+ * 
+ * @discussion
+ * This method is tied to the default run loop and is equivalent to -performSelector:withObject:afterDelay:.
+ */
 - (void)gx_performAfterDelay:(NSTimeInterval)delay block:(void (^)(void))block;
 
+
+/*!
+ * @method gx_associatedObjectForKey:
+ * @abstract Gets an associated object on the receiver
+ * 
+ * @param key
+ * The key used for the association
+ * 
+ * @result An object, or nil
+ */
 - (id)gx_associatedObjectForKey:(id <NSCopying>)key;
+
+/*!
+ * @method gx_setAssociatedObject:forKey:policy:
+ * @abstract Sets an associated object on the receiver
+ * 
+ * @param obj
+ * The object to add for the association
+ * 
+ * @param key
+ * The key to use for the association
+ * 
+ * @param policy
+ * The association policy to use
+ * 
+ * @discussion
+ * Pass nil for `obj` to remove an association.
+ * 
+ * If key is nil, this method raises an NSInvalidArgumentException.
+ */
 - (void)gx_setAssociatedObject:(id)obj forKey:(id <NSCopying>)key policy:(GXAssociationPolicy)policy;
+
+
+/*!
+ * @method gx_addObserver:forKeyPaths:options:context:
+ * @abstract Add a KVO observer for multiple key paths
+ *
+ * @param observer
+ * The object for which to add observations
+ *
+ * @param keyPaths
+ * The key paths for which to add observations
+ *
+ * @param context
+ * The context to use for the observations
+ *
+ * @discussion
+ * This is a convenience wrapper for adding a KVO observer for multiple key paths simultaneously
+ */
+- (void)gx_addObserver:(NSObject *)observer forKeyPaths:(NSSet *)keyPaths options:(NSKeyValueObservingOptions)options context:(void *)context;
+
+/*!
+ * @method gx_removeObserver:forKeyPaths:
+ * @abstract Remove a KVO observer for multiple key paths
+ *
+ * @param observer
+ * The object for which to remove observations
+ *
+ * @param keyPaths
+ * The key paths for which to remove observations
+ *
+ * @discussion
+ * This is a convenience wrapper for removing a KVO observer for multiple key paths simultaneously
+ */
+- (void)gx_removeObserver:(NSObject *)observer forKeyPaths:(NSSet *)keyPaths;
+
+/*!
+ * @method gx_removeObserver:forKeyPaths:context:
+ * @abstract Remove a KVO observer for multiple key paths
+ *
+ * @param observer
+ * The object for which to remove observations
+ *
+ * @param keyPaths
+ * The key paths for which to remove observations
+ *
+ * @param context
+ * The context used for the observations
+ *
+ * @discussion
+ * This is a convenience wrapper for removing a KVO observer for multiple key paths simultaneously
+ */
+- (void)gx_removeObserver:(NSObject *)observer forKeyPaths:(NSSet *)keyPaths context:(void *)context;
 
 @end
--- Graviton/Categories/NSObject+GravitonAdditions.m
+++ Graviton/Categories/NSObject+GravitonAdditions.m
@@ -11,6 +11,13 @@
 #import <objc/runtime.h>
 
 
+@interface NSObject (GravitonPrivateAdditions)
+
+- (void)gx_performBlock:(void (^)(void))block;
+
+@end
+
+
 @implementation NSObject (GravitonAdditions)
 
 - (void)gx_performBlock:(void (^)(void))block {
@@ -27,6 +34,24 @@
 
 - (void)gx_setAssociatedObject:(id)obj forKey:(id <NSCopying>)key policy:(GXAssociationPolicy)policy {
     objc_setAssociatedObject(self, (__bridge void *)key, obj, (objc_AssociationPolicy)policy);
+}
+
+- (void)gx_addObserver:(NSObject *)observer forKeyPaths:(NSSet *)keyPaths options:(NSKeyValueObservingOptions)options context:(void *)context {
+    for (NSString *keyPath in keyPaths) {
+        [self addObserver:observer forKeyPath:keyPath options:options context:context];
+    }
+}
+
+- (void)gx_removeObserver:(NSObject *)observer forKeyPaths:(NSSet *)keyPaths {
+    for (NSString *keyPath in keyPaths) {
+        [self removeObserver:observer forKeyPath:keyPath];
+    }
+}
+
+- (void)gx_removeObserver:(NSObject *)observer forKeyPaths:(NSSet *)keyPaths context:(void *)context {
+    for (NSString *keyPath in keyPaths) {
+        [self removeObserver:observer forKeyPath:keyPath context:context];
+    }
 }
 
 @end
